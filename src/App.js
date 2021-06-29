e2903f2e5d20142b779c49caf6ea5b3a170b009a
Shajib <sajidislam729@gmail.com>
2021-06-29 23:47:11 +0600

    update app

--- src/App.js
+++ src/App.js
@@ -34,8 +34,11 @@ const getLocalItems = () => {
 function App() {
 
   let [myArray,setMyArray]=useState(getLocalItems())
-  let [name,setName]=useState()
-  let [age,setAge]=useState()
+  let [name,setName]=useState('')
+  let [age,setAge]=useState('')
+  let [buttonStatus, setButtonStatus] =useState(true);
+  let [isEdit, setIsEdit] =useState(null);
+  let [editItem, setEditItem] =useState();
   
   const handleChange = (e) => {
     if (e.target.name === "name") {
@@ -47,64 +50,72 @@ function App() {
   
   }
 
+  // create item 
   const handleClick = () => {
-  setName("")
-  setAge("")
-  return setMyArray([...myArray,{id:4,myName:name,age:age}])
+    if (name !== "" && age !== "" && !isEdit) {
+      
+      setName("")
+      setAge("")
+  
+      const allInputData = {id:new Date().getTime().toString(),myName:name,age:age}
+      return setMyArray([...myArray, allInputData]);
+    } else if (name && age && isEdit) {
+      
+      setName("")
+      setAge("")
+  
+      setMyArray([...myArray.filter((elem)=> {
+        return isEdit!==elem.id
+        }),{id:new Date().getTime().toString(),myName:name,age:age}])
+      }
 }
   
+// Edit item 
+  const handleEdit = (id) => {
+    let newEditItem = myArray.find((elem) => {
+      return id===elem.id
+    })
+    setName(newEditItem.myName)
+    setAge(newEditItem.age)
 
+    setIsEdit(id)
+  }
+  
+// Delete item 
   const handleDelete = (id) => {
-  setMyArray(myArray.filter((v, i) => {
-    if (id !== i) {
+  setMyArray(myArray.filter((v) => {
+    if (id !== v.id) {
       return v;
     }
   })) 
   }
 
-  // #### screen width ####
-
-  // const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-  // const [screenHeight, setScreenHeight] = useState(window.innerHeight)
-
-  // const WHvalue = () => {
-  //   setScreenWidth(window.innerWidth)
-  //   setScreenHeight(window.innerHeight)
-  //   console.log(document.body.scrollHeight);
-  //   console.log(document.body.clientHeight);
-  // }
-
-  // useEffect(() => {
-  //   window.addEventListener('resize', WHvalue)
-
-  //   return () => {
-  //     console.log("remove Event");
-  //     window.removeEventListener("resize",WHvalue)
-  //   }
-    
-  // })
-
   // add localStorage 
   useEffect(() => {
-    localStorage.setItem('lists',JSON.stringify(myArray))
-  },[myArray])
+    localStorage.setItem('lists', JSON.stringify(myArray))
+    
+    if (name && age) {
+      setButtonStatus(false)
+    } else {
+      setButtonStatus(true)
+    }
+  },[myArray,name,age])
 
   return (
     <div className="App container">
-      {/* <h1>Width : {screenWidth }</h1>
-      <h1>Height : {screenHeight }</h1> */}
         <form action="#" onSubmit={e=>e.preventDefault()}>
           <input type="text" className="form-control w-50 my-3" placeholder="Enter Your Name" onChange={handleChange} name="name" value={name} />
           <input type="text" className="form-control w-50 my-3" placeholder="Enter Your Age" onChange={handleChange} name="age" value={age}/>
         
-        <button type='submit' className="btn btn-primary btn-lg" onClick={name!=="" && age!==""?handleClick:null}>Click</button>
+        <button type='submit' className="btn btn-primary btn-lg" onClick={name === "" && age === "" ?  null:handleClick } disabled={buttonStatus}>Submit</button>
         </form>
 
       {
-        myArray.map((list,i) => {
-          return <div className="item">
-          <h1 key={list.id}>{list.myName || "Your Name"} & Age: { list.age || "Your Age"}</h1>
-          <button className="btn btn-primary" onClick={()=>handleDelete(i)}>Delte</button>
+        myArray.map((list) => {
+          return <div className="item" key={list.id}>
+          <h1 key={list.id}>{list.myName || "Your Name"} & Age: { list.age || 'Your Age'}</h1>
+          <button className="btn btn-primary mx-2" onClick={()=>handleEdit(list.id)}>Edit</button>
+          <button className="btn btn-danger" onClick={()=>handleDelete(list.id)}>Delte</button>
           </div>
         })
       }
